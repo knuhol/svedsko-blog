3b0062d75fb25d53c7d5b55eec5e811ca0f91cc4
Knut Holm <knut.holm28@gmail.com>
2023-09-01 18:20:54 +0200

    feat: add error page 404

---
+++ template/components/NotFound.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+
+const NotFound = () => (
+  <section className="section-sm pb-0">
+    <div className="container" style={{ minHeight: 'calc(100vh - 335px)' }}>
+      <div className="row">
+        <div className="col-12">
+          <div className="text-center">
+            <h1 className="page-not-found-title">404</h1>
+            <p className="mb-4">Bohužel, tato stránka neexistuje. Zadali jste správnou adresu?</p>
+            <Link href="/" className="btn btn-primary">
+              Zpátky na hlavní stránku
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+)
+
+export { NotFound }
