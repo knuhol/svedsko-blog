ef31d3011a78ce99ae00747314733a754eb83ffb
Knut Holm <knut.holm28@gmail.com>
2023-09-03 23:50:36 +0200

    chore: Tina config

--- tina/config.js
+++ tina/config.js
@@ -124,25 +124,19 @@ const schema = defineSchema({
 export const config = defineConfig({
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
   branch:
-    process.env.NEXT_PUBLIC_TINA_BRANCH || // custom branch env override
-    process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF || // Vercel branch env
-    process.env.HEAD, // Netlify branch env
+    process.env.NEXT_PUBLIC_TINA_BRANCH ||
+    process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF ||
+    process.env.HEAD,
   token: process.env.TINA_TOKEN,
   media: {
-    // If you wanted cloudinary do this
-    // loadCustomStore: async () => {
-    //   const pack = await import("next-tinacms-cloudinary");
-    //   return pack.TinaCloudCloudinaryMediaStore;
-    // },
-    // this is the config for the tina cloud media store
     tina: {
       publicFolder: 'public',
       mediaRoot: 'uploads',
     },
   },
   build: {
-    publicFolder: 'public', // The public asset folder for your framework
-    outputFolder: 'admin', // within the public folder
+    publicFolder: 'public',
+    outputFolder: 'admin',
   },
   schema,
 })
